6c6dd4ed3e6ce3231ca7a51cab06b97b4d734044
Sam Gorick <samgorick.sg@gmail.com>
2020-03-13 16:13:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ def gross_for_director(director_data)
   movie_index = 0
   director_total = 0
   while movie_index < director_data[:movies].length
-    director_total += nds[director_data][:movies][movie_index][:worldwide_gross]
+    director_total += director_data[:movies][movie_index][:worldwide_gross]
     movie_index += 1
   end
   director_total
